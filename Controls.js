ee2a0bfc017ae842b894d815f70f27c615396c96
Jonas Grumann <j.grumann@bitmama.it>
2011-10-27 17:21:03 +0200

    recoded camera movements add added reset button" ©∂ quit "

--- Controls.js
+++ Controls.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 var gui = new DAT.GUI({
-    height : 4 * 32 - 1
+    height : 2 * 32 - 1
 });
-gui.add(params, 'posx').min(0).max(1000).step(1).name("PosX")
-gui.add(params, 'posy').min(0).max(2000).step(1).name("PosY")
-gui.add(params, 'posz').min(-1000).max(500).step(1).name("PosZ")
-gui.add(params, 'cubes').min(0).max(.9).step(0.1).name("Cubes")
+gui.add(params, 'maxcubes').min(100).max(20000).step(10).name("Max Cubes")
+gui.add(params, 'shrink').min(0.005).max(0.2).step(0.0025).name("1 / Trail length")
 })
