d64092d06d5586f393755f3ed80e6e8d43e977c9
Jonas Grumann <j.grumann@bitmama.it>
2011-11-02 12:00:47 +0100

    added readme file

--- Controls.js
+++ Controls.js
@@ -3,5 +3,5 @@ var gui = new DAT.GUI({
     height : 2 * 32 - 1
 });
 gui.add(params, 'maxcubes').min(100).max(20000).step(10).name("Max Cubes")
-gui.add(params, 'shrink').min(0.005).max(0.2).step(0.0025).name("1 / Trail length")
+gui.add(params, 'shrink').min(10).max(300).step(0.0025).name("Trail length")
 })
---
+++ readme.txt
@@ -0,0 +1,7 @@
+This is another test using Threejs, webgl and jquery.
+
+Check it out on http://www.pataphysicalturkey.ch/experiments/star
+
+I only tested it on chrome and ff.
+
+Have a nice day.
